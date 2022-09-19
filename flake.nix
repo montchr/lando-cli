57b9981b7f082d0675803c3b37f15b19b13a8deb
Chris Montgomery <chris@cdom.io>
2022-09-19 13:02:04 -0400

    add a nixpkgs overlay output

--- flake.nix
+++ flake.nix
@@ -1,9 +1,11 @@
 {
   inputs.dream2nix.url = "github:nix-community/dream2nix";
   outputs = inp:
-    inp.dream2nix.lib.makeFlakeOutputs {
+    (inp.dream2nix.lib.makeFlakeOutputs {
       systems = ["x86_64-linux"];
       config.projectRoot = ./.;
       source = ./.;
-    };
+    }) // {
+      overlays.default = final: prev: {lando-cli = inp.self.packages.${prev.system}."@lando/cli";};
+};
 }
