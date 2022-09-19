f233ce3bc4210376e9138c8f866c067bb08a555e
Chris Montgomery <chris@cdom.io>
2022-09-19 14:24:01 -0400

    nix: various tweaks

--- flake.nix
+++ flake.nix
@@ -1,11 +1,17 @@
 {
   inputs.dream2nix.url = "github:nix-community/dream2nix";
-  outputs = inp:
-    (inp.dream2nix.lib.makeFlakeOutputs {
+  outputs = inp@{self,dream2nix,...}:
+    (dream2nix.lib.makeFlakeOutputs {
       systems = ["x86_64-linux"];
       config.projectRoot = ./.;
       source = ./.;
+      settings = [
+{
+            subsystemInfo.noDev = true;
+}
+];
     }) // {
-      overlays.default = final: prev: {lando-cli = inp.self.packages.${prev.system}."@lando/cli";};
+      overlays.default = final: prev: {lando-cli = self.packages.${prev.system}."@lando/cli";};
+checks = self.packages;
 };
 }
